57a9903685b2d93c356fd62de479f77f626e6cfe
Kurtis Aspden <kurtisga@gmail.com>
2020-08-31 21:38:11 +0100

    Add project files.

---
+++ BANKIR/win_Home.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace BANKIR
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class win_Home : Window
+    {
+        public win_Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
